a51d08a4302642e1afcfec04f2314da09e6f4856
OmarGad11 <57959996+OmarGad11@users.noreply.github.com>
2019-11-21 14:21:53 +0100

    Add files via upload

---
+++ hotel2.java
@@ -0,0 +1,12 @@
+package SquareRoot;
+
+import java.util.*;
+
+public class hotel2 {
+	public static void main(String args[]) {
+
+		String bill = ("The bill for 1x night is ");
+		double mydouble = 85.000000000;
+		System.out.printf("%-40s %.2f" , bill, mydouble);
+	}
+}
